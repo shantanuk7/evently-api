989073895146fa6f9ffcdb20cf8b0f3c5148d025
shantanuk7 <shantanukulkarni365@gmail.com>
2026-03-07 23:23:43 +0530

    feat: add security config with public and protected endpoints settings

---
+++ src/main/java/com/springproject/eventmanagementsystem/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.springproject.eventmanagementsystem.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        // Public endpoints
+                        .requestMatchers("/api/auth/**").permitAll()
+
+                        // Protected endpoints
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+}
